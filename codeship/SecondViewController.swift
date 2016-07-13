8014a8cdc8c62d7fddc106cb242c86cecc10e3ea
Patrick Sullivan <patrick@pdsullivan.com>
2016-07-12 18:38:18 -0700

    adds xcode project

---
+++ codeship/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  codeship
+//
+//  Created by Patrick Sullivan on 7/12/16.
+//  Copyright © 2016 pdsullivan. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
